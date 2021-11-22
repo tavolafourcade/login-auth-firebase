6f4dc116ea7c1107fe78298bb7bdc21ebc7392b5
octaviolafourcade <octavio.lafourcade@gmail.com>
2021-11-21 19:14:24 -0500

    [ ADD ] Input validation

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,22 +1,48 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Login = () => {
+
+    const [email, setEmail] = useState('')
+    const [pass, setPass] = useState('')
+
+    const procesarDatos = (e) => {
+        e.preventDefault()
+        if(!email.trim()){
+            console.log('Ingrese un email')
+            return
+        }
+
+        if(!pass.trim()){
+            console.log('Ingrese una contraseña')
+            return
+        }
+
+        if(pass.length < 6){
+            console.log('La contraseña debe tener al menos 6 caracteres')
+            return
+        }
+    }
+
     return (
         <div className='mt-5'>
-            <h3 className="text-center">Acceso o Registro de Usuarios</h3>
+            <h3 className="text-center">Registro de Usuarios</h3>
             <hr />
             <div className="row justify-content-center">
                 <div className="col-12 col-sm-8 col-md-6 col-xl-4">
-                    <form>
+                    <form onSubmit={procesarDatos}>
                     <input 
                         type="email" 
                         className="form-control mb-2"
-                        placeholder="Ingrese un email"/>
+                        placeholder="Ingrese un email"
+                        onChange={e => setEmail(e.target.value)}
+                        value={email}/>
 
                     <input 
                         type="password" 
                         className="form-control mb-2"
-                        placeholder="Ingrese una contraseña"/>
+                        placeholder="Ingrese una contraseña"
+                        onChange={e => setPass(e.target.value)}
+                        value={pass}/>
                     <button className="btn btn-dark btn-lg btn-block">
                         Registrarme
                     </button>
